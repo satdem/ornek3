81a991398c6c41ffa523e589692418309ef24ac0
satı demirci <69872891+satdem@users.noreply.github.com>
2020-11-24 20:33:46 +0100

    sepete ekle kısmı eklendi

---
+++ src/test/java/deneme.java
@@ -0,0 +1,11 @@
+public class deneme {
+    public static void main(String[] args) {
+        System.out.println("seni seni");
+    }
+
+    public static void yeniMetod(){
+        System.out.println("yeni metod hazır");
+        System.out.println("sepete ekle");
+    }
+}
+
